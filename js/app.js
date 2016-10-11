dffed3935a454491959eadd782fa25b8a2286aae
gi <gise11e@me.com>
2016-10-11 09:49:13 +0100

    refactored the code (games as objects), added a splash screen

--- js/app.js
+++ js/app.js
@@ -1,203 +1,236 @@
+$(function() {
 
+	var screens = {};
+	var currentScreen = null;
 
-var gameBoard = document.getElementById('game-board');
-
-var cards = [];
-
-var level = 0;
-
-var levelPairs = [3,5,7];
-
-var cardTypes = ["im1", "im2", "im3", "im4","im5", "im6", "im7"];
-
-var cardsInPlay = [];
-
-var safeToPlay = true;
-
-var totalMatches = 0;
-
-var messageDiv = document.getElementById('message');
-
-var createBoard = function() {
-
-	for (var i = 0; i < cards.length; i++) {
-
-		// Create the div for the card
-		var cardElement = document.createElement('div');
-		cardElement.className = "card";
-
-		// Add the div to the gameboard
-		gameBoard.appendChild(cardElement);
-
-		cardElement.setAttribute('data-card', cards[i]);
-		cardElement.addEventListener('click', isTwoCards);
-	}
-};
-
-function setUpCards(totalPairs){
-	cards = [];
-	for  (var i=0; i< totalPairs; i++){
-		cards.push(cardTypes[i % cardTypes.length]);
-		cards.push(cardTypes[i % cardTypes.length]);
-	}
-}
-
-function isMatch(selectedCards) {
-
-	if( selectedCards[0].getAttribute('data-card') == selectedCards[1].getAttribute('data-card') )
-		return true;
-	else return false;
-}
-
-var won = false;
-
-function resetBoard() {
-	if(won){
-		confetti.clear();
-	}
-	won = false;
-	var bodyElements = document.getElementsByTagName('body');
-	bodyElements[0].className = 'level-' + level;
-	gameBoard.innerHTML='';
-	setUpCards(levelPairs[level]);
-	shuffle(cards);
-	createBoard();
-
-	totalMatches = 0;
-	messageDiv.innerHTML = '';
-    var cardsOnBoard = document.querySelectorAll(".card");
-    for (var i=0; i<cardsOnBoard.length; i++) {
-    	cardsOnBoard[i].innerHTML = "";
-    	cardsOnBoard[i].setAttribute('data-card', cards[i]);
-    }
-}
-
-var glissando = new Audio("sounds/harp_chord_glissando.mp3");
-
-var matchSound = new Audio ("sounds/match2.mp3");
-
-var winGame = new Audio ("sounds/win.mp3");
-
-
-function isTwoCards() {
-
-  if (!safeToPlay) {
-  	return;
-  }
-
-  var cardElement = this;
-
-  var typeOfCard = cardElement.getAttribute('data-card');
-
-	if (cardsInPlay.length == 1 && cardElement === cardsInPlay[0]) {
-		return;
-	}
-
-  cardsInPlay.push(cardElement);
-
-  cardElement.innerHTML = '<img src="images/' + typeOfCard + '.gif" alt="' + typeOfCard + ' playing card"/>';
-
-  if (cardsInPlay.length == 2) {
-
-    if (isMatch(cardsInPlay)) {
-
-			cardsInPlay[0].removeEventListener('click', isTwoCards);
-			cardsInPlay[1].removeEventListener('click', isTwoCards);
-
-    	messageDiv.innerHTML = "whoop whoop!";
-			matchSound.play();
-    	cardsInPlay = [];
-
-    	totalMatches += 1;
-
-    	if (totalMatches == (levelPairs[level])) {
-
-
-				if (level == levelPairs.length-1) {
-					messageDiv.innerHTML = "<h2 class= 'win' >  h o o r r a y! </h2>";
-					winGame.play();
-					confetti.start();
-					won =  true;
-					glissando.play();
-					transitionOut();
-					setTimeout(function() {
-						confetti.clear();
-						messageDiv.innerHTML = "";
-						$('#game-board').hide();
-						$('.grid').hide();
-						$('#sidebar').hide();
-						$('#game-2').removeClass('hidden');
-					}, 7000);
-
-	}
-				else {
-				  messageDiv.innerHTML = '<div class="levelup"> level ^ ! </div>';
-					level++;
-					transitionOut(resetBoard);
-					glissando.play();
-				}
-    	}
-    }
-		    else {
-
-    	messageDiv.innerHTML = "&#x0727;";
-
-    	safeToPlay = false;
-
-		// turn both cards back over after 1 second (1000ms).
-    	setTimeout(function() {
-    		cardsInPlay[0].innerHTML = '';
-    		cardsInPlay[1].innerHTML = '';
-		    cardsInPlay = [];
-		    safeToPlay = true;
-    	}, 1000);
-    }
-  }
-}
-
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-}
-
-resetBoard();
-
-for (var i=1; i<3; i++) {
-	document.getElementById('level'+(i+1)).addEventListener('click', (function(l){
-		return function() {
-			level=l; resetBoard();
-		};
-	})(i));
-}
-
-document.getElementById('reset').addEventListener('click', function() { level=0;resetBoard();});
-
-
-function transitionOut(callback) {
-	var $lis = $(gameBoard).children();
-	var total = $lis.length;
-	$.each($lis,function(i,li){
-		if (i == total-1){
-			$(li).fadeOut(500*(i+1), callback);
+	function addScreen(id, screenObject) {
+		screens[id] = screenObject;
+		// Invoke the setup method if it exists.
+		if (typeof screenObject.setup === 'function') {
+			screenObject.setup();
 		}
-		else{
-			$(li).fadeOut(500*(i+1));
+	}
+
+	function transitionTo(screenId) {
+		if (currentScreen !== null) {
+			currentScreen.onLeave();
+		}
+		currentScreen = screens[screenId];
+		currentScreen.onEnter();
+	}
+
+	addScreen('splash', {
+		blinkSpan:null,
+		splashDiv: null,
+		setup: function() {
+			this.blinkSpan = $('#blinking');
+			this.splashDiv = $('#splash');
+			this.blinkSpan.on('click', function() {
+				transitionTo('memoryGame');
+			});
+		},
+		onEnter: function() {
+			this.blinkSpan.show();
+			this.splashDiv.show();
+
+		},
+		onLeave: function() {
+			this.blinkSpan.hide();
+			this.splashDiv.hide();
+
 		}
 	});
-}
+
+	addScreen('memoryGame', {
+		gameBoard: $('#game-board'),
+		message: $('#message'),
+		grid: $('.grid'),
+		sidebar: $('#sidebar'),
+		cards: [],
+		level: 0,
+		levelPairs: [3, 5, 7],
+		cardTypes: ["im1", "im2", "im3", "im4","im5", "im6", "im7"],
+		cardsInPlay: [],
+		safeToPlay: true,
+		totalMatches: 0,
+		hasWon: false,
+
+		sounds: {
+			glissando: new Audio("sounds/harp_chord_glissando.mp3"),
+			matchSound: new Audio ("sounds/match2.mp3"),
+			winGame: new Audio ("sounds/win.mp3")
+		},
+
+		shuffle: function(arr) {
+		  var currentIndex = arr.length, temporaryValue, randomIndex;
+		  // While there remain elements to shuffle...
+		  while (0 !== currentIndex) {
+		    // Pick a remaining element...
+		    randomIndex = Math.floor(Math.random() * currentIndex);
+		    currentIndex -= 1;
+		    // And swap it with the current element.
+		    temporaryValue = arr[currentIndex];
+		    arr[currentIndex] = arr[randomIndex];
+		    arr[randomIndex] = temporaryValue;
+		  }
+		  return arr;
+		},
+
+		createBoard: function() {
+			for (var i = 0; i < this.cards.length; i++) {
+				var cardElement = $('<div></div>').addClass('card').attr('data-card', this.cards[i]);
+
+				cardElement.on('click', (function(that){
+					return function() {
+						that.isTwoCards.apply(that, $(this));
+					};
+				})(this));
+				this.gameBoard.append(cardElement);
+			}
+		},
+
+		setUpCards: function(totalPairs) {
+			this.cards = [];
+			for  (var i=0; i< totalPairs; i++){
+				this.cards.push(this.cardTypes[i % this.cardTypes.length]);
+				this.cards.push(this.cardTypes[i % this.cardTypes.length]);
+			}
+		},
+
+		isMatch: function(selectedCards) {
+			return selectedCards[0].attr('data-card') == selectedCards[1].attr('data-card');
+		},
+
+		resetBoard: function() {
+			if (this.hasWon) {
+				confetti.clear();
+			}
+			this.hasWon = false;
+			$('body')[0].className = 'level-' + this.level;
+			this.gameBoard.html('');
+			this.setUpCards(this.levelPairs[this.level]);
+			this.shuffle(this.cards);
+			this.createBoard();
+			this.totalMatches = 0;
+			this.message.html('');
+
+			var cardsOnBoard = $('.card');
+			var that = this;
+			$.each(cardsOnBoard, function(i, card) {
+				$(card).html('');
+				$(card).attr('data-card', that.cards[i]);
+			});
+		},
+
+		transitionCards: function(callback) {
+			var $lis = this.gameBoard.children();
+			var total = $lis.length;
+			$.each($lis,function(i,li){
+				if (i == total-1){
+					$(li).fadeOut(500*(i+1), callback);
+				}
+				else{
+					$(li).fadeOut(500*(i+1));
+				}
+			});
+		},
+
+		isTwoCards: function(cardElement) {
+
+			var cardElement = $(cardElement);
+
+			if (!this.safeToPlay) {
+				return;
+			}
+
+			var typeOfCard = cardElement.attr('data-card');
+
+			// Don't let a card match against itself.
+			if (this.cardsInPlay.length == 1 && cardElement.get(0) === this.cardsInPlay[0].get(0)) {
+				return;
+			}
+
+			this.cardsInPlay.push(cardElement);
+
+			cardElement.html('<img src="images/' + typeOfCard + '.gif" alt="' + typeOfCard + ' playing card"/>');
+
+			if (this.cardsInPlay.length == 2) {
+				if (this.isMatch(this.cardsInPlay)) {
+					// Remove click handlers of matched cards.
+					this.cardsInPlay[0].off('click');
+					this.cardsInPlay[1].off('click');
+					this.message.html("whoop whoop!");
+
+					this.sounds.matchSound.play();
+					this.cardsInPlay = [];
+
+					// Increment the total matches.
+					this.totalMatches++;
+
+					if (this.totalMatches == this.levelPairs[this.level]) {
+
+
+						// Last level
+						if (this.level == this.levelPairs.length - 1) {
+							this.message.html('<h2 class="win">h o o r a y!</h2>');
+							this.sounds.winGame.play();
+							this.sounds.glissando.play();
+							confetti.start();
+							this.hasWon = true;
+							this.transitionCards(this.resetBoard.bind(this));
+						}
+						else {
+						  this.message.html('<div class="levelup"> level ^ ! </div>');
+							this.level++;
+							this.transitionCards(this.resetBoard.bind(this));
+							this.sounds.glissando.play();
+						}
+					}
+				}
+				else {
+					this.message.html("");
+		    	this.safeToPlay = false;
+
+					// turn both cards back over after 1 second (1000ms).
+		    	setTimeout((function(that) {
+						return function() {
+							that.cardsInPlay[0].html('');
+							that.cardsInPlay[1].html('');
+							that.cardsInPlay = [];
+							that.safeToPlay = true;
+						};
+					})(this), 1000);
+				}
+			}
+		},
+
+		setup: function() {
+			var that = this;
+			for (var i=1; i<3; i++) {
+				$('#level'+(i+1)).on('click', (function(l){
+					return function() {
+						that.level=l; that.resetBoard();
+					};
+				})(i));
+			}
+			$('#reset').on('click', function() { that.level = 0; that.resetBoard(); });
+		},
+
+		onEnter: function() {
+			this.resetBoard();
+		},
+
+		onLeave: function () {
+			this.gameBoard.hide();
+			this.grid.hide();
+			this.sidebar.hide();
+		}
+	});
+
+	transitionTo('splash');
+
+});
 
 /////GAME 2
+
+// var chooseUniverse = new textField("choose universe");
